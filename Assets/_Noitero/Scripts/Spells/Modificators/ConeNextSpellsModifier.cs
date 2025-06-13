ac9e163dcc997614ede39d8cd5d47e86b9ea5d79
Sebastien chevallier <44536776+SebastienChevallier@users.noreply.github.com>
2025-06-13 10:10:46 +0200

    Allow dynamic cone shot count

---
+++ Assets/_Noitero/Scripts/Spells/Modificators/ConeNextSpellsModifier.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Spells/Modifier/Cone Next Spells")]
+public class ConeNextSpellsModifier : SpellBase
+{
+    [SerializeField] private int defaultShots = 3;
+    [SerializeField] private float coneAngle = 30f;
+    [SerializeField] private string shotCountVariable = "coneShots";
+
+    public override void Execute(SpellExecutionContext context)
+    {
+        int shots = context.GetVariable<int>(shotCountVariable);
+        if (shots <= 0)
+            shots = defaultShots;
+
+        int castCount = Mathf.Min(shots, context.RemainingSpells.Count);
+        if (castCount <= 0)
+            return;
+
+        float step = (castCount > 1) ? coneAngle / (castCount - 1) : 0f;
+        Vector3 baseDir = context.Direction;
+        int originIndex = context.ExecutedSpellIndex;
+
+        List<GameObject> totalProjectiles = new();
+
+        for (int i = 0; i < castCount; i++)
+        {
+            SpellBase spell = context.RemainingSpells[0];
+
+            float offset = -coneAngle / 2f + step * i;
+            context.Direction = Quaternion.AngleAxis(offset, Vector3.up) * baseDir;
+            context.ExecutedSpellIndex = originIndex + i + 1;
+            context.RemainingSpells = context.RemainingSpells.Skip(1).ToList();
+            context.SpawnedProjectiles = new List<GameObject>();
+
+            spell.Execute(context);
+
+            totalProjectiles.AddRange(context.SpawnedProjectiles);
+            context.AdvanceIndexAction?.Invoke(context.ExecutedSpellIndex + 1);
+        }
+
+        context.SpawnedProjectiles = totalProjectiles;
+    }
+}
